4861500fc614c5d99d7637636851c916ac65aefc
Juan Bertinetti <juanbertinetti@gmail.com>
2015-10-28 17:41:11 -0300

    Agregado soporte para mp3 a togg

--- togg.sh
+++ togg.sh
@@ -6,6 +6,14 @@ if [ ! $# == 1 ]; then
 fi
 ext=$1
 mkdir -p ogg
+
+if [ $ext == 'mp3' ]; then
+    for i in *.mp3; do
+        lame --decode "$i" "$i".wav
+    done
+    ext="wav"
+fi
+
 for i in *.$ext; do
     oggenc "$i" -q 6 -o ogg/"$i".ogg &
 done
